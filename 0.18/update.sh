af9d9763291fb1b9a29d43006bc6d45048b5e1e7
devfans <stefanliu@outlook.com>
2019-07-26 15:22:02 +0800

    init commit to add 0.18 bitcoin core container

---
+++ 0.18/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t devfans/bitcoin:0.18 .
+# docker push devfans/bitcoin:0.18
+# docker tag devfans/bitcoin:0.18 devfans/bitcoin:0.18.0
+# docker push devfans/bitcoin:0.18.0
